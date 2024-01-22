76b4f916f7bf77d939b0bf18d2766b63e5b33593
Nayatha Yasiru <Nayantha.Koralegama@sysco.com>
2024-01-22 10:02:05 +0530

    reformat the code.

--- src/main/java/org/syscolabs/coe/Main.java
+++ src/main/java/org/syscolabs/coe/Main.java
@@ -1,5 +1,1 @@
-package org.syscolabs.coe;
-public class Main
-{ public static void main(String[] args)
-{System.out.println("Hello world!");}
-}
+package org.syscolabs.coe;public class Main { public static void main(String[] args) {System.out.println("Hello world!");}}
